348e7fa4af6d94c2eaf8c666bba4e3f1a93eea40
Aditya Kakad <202052302@iiitvadodara.ac.in>
2022-05-30 18:03:50 +0530

    new changes

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+postRestaurant(data: any){
+  return this.http.post<any>("https://localhost:3000/posts",data).pipe(map((res:any)=>{
+    return res;
+  }))
+}
+
+getRestaurant(data: any){
+return this.http.get<any>("https://localhost:3000/posts",data).pipe(map((res:any)=>{
+  return res;
+}))
+}
+
+updateRestaurant(data: any,id:number){
+  return this.http.put<any>("https://localhost:3000/posts"+id,data).pipe(map((res:any) => {
+    return res; 
+  }))
+}
+
+deleteRestaurant(id:number){
+return this.http.delete<any>("https://localhost:3000/posts"+id).pipe(map((res:any) => {
+  return res;
+}))
+}
+}
